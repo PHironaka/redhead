63bf893f2472cbb0cd1c1b3de89144e4b3c56716
Peter Hironaka <peter.hironaka@macc02q34kcg8wl.home>
2015-10-20 23:06:03 -0700

    Automated commit at 2015-10-21 06:06:03 UTC by middleman-deploy 1.0.0

---
+++ javascripts/videos.js
@@ -0,0 +1,17 @@
+var videos = [
+
+
+    {
+      title: "Jimmy Napes",
+      url: "/videos/Jimmy-Napes",
+      slug: "Jimmy-Napes",
+      youtube: "jsRJPJrOnVo"
+    },
+
+		{
+      title: "White Noise",
+      url: "/videos/white-noise",
+      slug: "white-noise",
+      vimeo: "60021080"
+    },
+];
